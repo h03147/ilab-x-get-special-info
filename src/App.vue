4ad347f4b8ba9dc32277a609252cce13c9cd2f20
arthurye <soyellowjack@gmail.com>
2021-02-28 15:00:15 +0800

    version: 1.1.1 search input box was replaced with select search

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
   <div id="app">
-    <el-row :gutter="20" class="elrow1" inline="true">
-      <el-col :span="6">
-        <el-input v-model="input1" size="small" placeholder="请输入分类编号">
-
-        </el-input>
+    <el-row class="elrow_header1">
+      <el-col class="elcoldiv1">
+        <span class="headerspan1">ilab-x.com 虚拟仿真实验教学共享平台的数据分析</span>
       </el-col>
-      <el-col :span="6">
-        <el-input v-model="input2" size="small" placeholder="请输入起始页">
+    </el-row>
+    <el-row :gutter="24" class="elrow1" inline="true">
+      <el-col :span="12" class="elrow1_col1">
+<!--        <el-input v-model="input1" size="small" placeholder="请输入分类编号">-->
 
-        </el-input>
+<!--        </el-input>-->
+        <el-select size="small"
+                   class="elselect1"
+                   v-model="menuSelect"
+                   filterable placeholder="请选择"
+                   @change="currStationChange">
+          <el-option
+                  v-for="item in catalogData"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item">
+            <span style="float: left">{{ item.label }}</span>
+            <span style="float: right; color: #48C9B0; font-size: 13px">总数:{{ item.totalNumber }}</span>
+            <span style="float: right; color: #FF5733; font-size: 13px; margin-right: 10px;">编号:{{ item.value }}</span>
+          </el-option>
+        </el-select>
       </el-col>
-      <el-col :span="6">
-        <el-input v-model="input3" size="small" placeholder="请输入显示数量">
+<!--      <el-col :span="6">-->
+<!--        <el-input v-model="input2" size="small" placeholder="请输入起始页">-->
 
-        </el-input>
-      </el-col>
-      <el-col :span="3">
+<!--        </el-input>-->
+<!--      </el-col>-->
+<!--      <el-col :span="6">-->
+<!--        <el-input v-model="input3" size="small" placeholder="请输入显示数量">-->
+
+<!--        </el-input>-->
+<!--      </el-col>-->
+      <el-col :span="2">
         <el-button type="primary" size="small" @click="filterSearch">
             进行查询
         </el-button>
       </el-col>
-      <el-col :span="3">
+      <el-col :span="2">
         <el-button type="success"
                    size="small"
                     @click="exportExcel">
@@ -33,8 +53,31 @@
     <el-table
             class="eltable1"
             :data="searchTableData">
+      <el-table-column
+              type="index"
+              width="50">
+      </el-table-column>
       <el-table-column label="项目学校" prop="schoolTitle"></el-table-column>
       <el-table-column label="项目名称" prop="title"></el-table-column>
+      <el-table-column label="项目负责人姓名" prop="userInfo.name"></el-table-column>
+      <el-table-column label="专业技术职称" prop="userInfo.techDuty">
+        <template slot-scope="scope">
+          <el-tag type="success" v-if="scope.row.userInfo.techDuty == 4">{{techDutyData[4]}}
+          </el-tag>
+          <el-tag v-else-if="scope.row.userInfo.techDuty == 3">
+            {{techDutyData[3]}}
+          </el-tag>
+          <el-tag type="info" v-else-if="scope.row.userInfo.techDuty == 2">
+            {{techDutyData[2]}}
+          </el-tag>
+          <el-tag type="danger" v-else-if="scope.row.userInfo.techDuty == 1">
+            {{techDutyData[1]}}
+          </el-tag>
+          <el-tag type="warning" v-else>{{techDutyData[0]}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="行政职务" prop="userInfo.adminDuty"></el-table-column>
+      <el-table-column label="所属院系" prop="userInfo.faculty"></el-table-column>
 
     </el-table>
   </div>
@@ -49,8 +92,26 @@
     return {
       searchTableData: [],
       input1: '',
-      input2: null,
+      input2: 1,
       input3: null,
+      catalogData: [
+        {label: '法学类', value: 217, totalNumber: 15}, {label: '建筑类', value: 227, totalNumber: 21}, {label: '马克思主义理论学', value: 218, totalNumber: 14},
+        {label: '体育学类', value: 219, totalNumber: 27}, {label: '历史学类', value: 220, totalNumber: 19}, {label: '物理学类', value: 221, totalNumber: 53},
+        {label: '电气类', value: 222, totalNumber: 64}, {label: '矿业类', value: 223, totalNumber: 35}, {label: '兵器类', value: 224, totalNumber: 20},
+        {label: '农业工程类', value: 225, totalNumber: 22}, {label: '林业工程类', value: 226, totalNumber: 9}, {label: '自然保护与环境生态类', value: 228, totalNumber: 27},
+        {label: '公共卫生与预防医学类', value: 229, totalNumber: 26}, {label: '法医学类', value: 230, totalNumber: 11}, {label: '医学技术类', value: 231, totalNumber: 30},
+        {label: '经济管理类', value: 232, totalNumber: 139}, {label: '艺术学类', value: 233, totalNumber: 67}, {label: '生物科学类', value: 0, totalNumber: 73},
+        {label: '机械类', value: 1, totalNumber: 173}, {label: '电子信息类', value: 2, totalNumber: 63}, {label: '航空航天类', value: 86, totalNumber: 34},
+        {label: '化工与制药类', value: 3, totalNumber: 118}, {label: '交通运输类', value: 4, totalNumber: 60}, {label: '核工程类', value: 5, totalNumber: 22},
+        {label: '临床医学类', value: 6, totalNumber: 97}, {label: '药学类', value: 7, totalNumber: 50}, {label: '化学类', value: 80, totalNumber: 119},
+        {label: '心理学类', value: 81, totalNumber: 12}, {label: '能源动力类', value: 82, totalNumber: 37}, {label: '土木类', value: 83, totalNumber: 119},
+        {label: '测绘类', value: 84, totalNumber: 15}, {label: '环境科学与工程类', value: 87, totalNumber: 30}, {label: '食品科学与工程类', value: 88, totalNumber: 31},
+        {label: '植物类', value: 89, totalNumber: 64}, {label: '动物类', value: 90, totalNumber: 56}, {label: '基础医学类', value: 91, totalNumber: 124},
+        {label: '中医类', value: 92, totalNumber: 43}, {label: '护理学类', value: 93, totalNumber: 26}, {label: '教育学类', value: 94, totalNumber: 34},
+        {label: '地质类', value: 85, totalNumber: 20}, {label: '文学类', value: 95, totalNumber: 60}
+      ],
+      menuSelect: '',
+      techDutyData: ['高级工程师/研究员', '未知', '讲师', '副教授', '教授'],
     }
   },
   created() {
@@ -143,18 +204,64 @@
     formatJson(filterVal, jsonData) {//循环重组数据
       return jsonData.map(v => filterVal.map(j => v[j]))
     },
+    // 下拉选择框的监听
+    currStationChange(item) {
+      // console.log(item.label);
+      // this.$message(item.label + item.value + item.totalNumber);
+      this.input1 = item.value;
+      this.input3 = item.totalNumber;
+      this.$notify({
+        title: '选择成功',
+        message: '当前选择的分类编号为:' + item.value + '，共有' + item.totalNumber + '条',
+        type: 'success',
+        position: 'bottom-right'
+      });
+    },
   }
 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body,#app{
+    height: 100%!important;
+    padding: 0;
+    margin: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  /* 下拉框elselect1样式*/
+  .elselect1 {
+    width: 100%;
+  }
+
+  /*header里面左边字的样式*/
+  .headerspan1 {
+    width: 7.6vw;
+    height: 1.7vh;
+    font-size: 1.8vw;
+    font-family: Source Han Sans SC;
+    font-weight: 400;
+    color: #FFFFFF;
+    line-height: 47px;
+    margin-left: 1vw;
+
+  }
+
+  /* 标题行的背景颜色*/
+  .elrow_header1 {
+    width: 100%;
+    background-color: #457EDD;
+  }
+
+  /* 选择搜索行样式*/
+  .elrow1 {
+    margin: 1vh;
+  }
+
+  /* 下拉选择框*/
+  .elrow1_col1 {
+    padding-left: 1.2vw !important;
+  }
 </style>
